8b7bd225d0e2515f52707fe7cfcaa39e54b7a9f2
Vladislav Samorukov <smrk.vld@gmail.com>
2023-02-10 18:42:20 +0300

    done

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        double a, b, result;
+        a = b = 0;
+        result = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
